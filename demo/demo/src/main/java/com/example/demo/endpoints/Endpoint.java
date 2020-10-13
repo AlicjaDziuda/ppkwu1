3a2412337cb338ddb555d500065ff31125110332
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-10-13 16:17:09 +0200

    Endpoint gotowy

--- demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
+++ demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -1,10 +1,26 @@
 package com.example.demo.endpoints;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/restapi/")
 public class Endpoint {
+    private Service service;
 
+    //wstrzykiwanie obiektu Service
+    @Autowired
+    public Endpoint(Service service) {
+        this.service = service;
+    }
+
+    //@PathVariable - przekazywanie zmiennych przez url
+    //@GetMapping  - przyjumuje jako arg ścieżkę żądania
+    @GetMapping("/{characters}")
+    public String getResult(@PathVariable String characters) {
+        return service.reverseCharacters(characters);
+    }
 }
--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -7,5 +7,5 @@ public class Service {
         StringBuilder string = new StringBuilder(characters).reverse();
         return string.toString();
     }
-    
+
 }
